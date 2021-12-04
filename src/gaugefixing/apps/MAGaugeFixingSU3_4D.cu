0d562caa3d649283cda84901310e909e4030345a
Ilya <ilyakudrov@yandex.ru>
2021-12-04 20:32:38 +0300

    fixed copying to host

--- src/gaugefixing/apps/MAGaugeFixingSU3_4D.cu
+++ src/gaugefixing/apps/MAGaugeFixingSU3_4D.cu
@@ -322,30 +322,6 @@ int main(int argc, char* argv[])
 			//gaugeStats.generateGaugeQuality();
                         output << copy<<","<<gaugeStats.getCurrentGff()<<endl;
 
-			if(copy < options.getGaugeCopies() - 1 && options.getSaveEach()){
-				stringstream filename(stringstream::out);
-				filename << fi.getOutputFilename() << "_" << copy + 1;
-				string copy_path = filename.str();
-				switch( options.getFType() ) {
-					case VOGT:
-						loadOk = lfVogt.save( s, copy_path , U );
-						break;
-					case PLAIN:
-						loadOk = lfPlain.save( s, copy_path, U );
-						break;
-					case HEADERONLY:
-						loadOk = lfHeaderOnly.save( s, copy_path, U );
-						break;
-					case ILDG:
-						loadOk = true;
-						writeILDG(s, fi.getFilename().c_str(), (copy_path).c_str(), HOST_CONSTANTS::SIZE, U, options.getSaSteps());
-						break;
-					default:
-						cout << "Filetype not set to a known value. Exiting";
-						exit(1);
-				}
-			}
-
 			// check for best copy
 			if( gaugeStats.getCurrentGff() > bestGff )
 			{
@@ -358,6 +334,35 @@ int main(int argc, char* argv[])
 			else
 			{
 				cout << "NO BETTER COPY" << endl;
+			}
+
+			if(copy < options.getGaugeCopies() - 1 && options.getSaveEach()){
+				Real* U1 = (Real*)malloc( arraySize*sizeof(Real) );
+				cudaMemcpy( U1, dU, arraySize*sizeof(Real), cudaMemcpyDeviceToHost );
+
+				stringstream filename(stringstream::out);
+				filename << fi.getOutputFilename() << "_" << copy + 1;
+				string copy_path = filename.str();
+				switch( options.getFType() ) {
+					case VOGT:
+						loadOk = lfVogt.save( s, copy_path , U1 );
+						break;
+					case PLAIN:
+						loadOk = lfPlain.save( s, copy_path, U1 );
+						break;
+					case HEADERONLY:
+						loadOk = lfHeaderOnly.save( s, copy_path, U1 );
+						break;
+					case ILDG:
+						loadOk = true;
+						writeILDG(s, fi.getFilename().c_str(), (copy_path).c_str(), HOST_CONSTANTS::SIZE, U1, options.getSaSteps());
+						break;
+					default:
+						cout << "Filetype not set to a known value. Exiting";
+						exit(1);
+				}
+
+				free(U1);
 			}
 		}
 
